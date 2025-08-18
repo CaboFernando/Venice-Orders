c2097c6c131091cb99c16213d1fe96a840350be3
Carlos Fernando dos Santos <fer.1972@hotmail.com>
2025-08-18 09:17:09 -0300

    Upload via github
    Aconteceu algo no meu Visual Studio e o mesmo não está reconhecendo a criação do arquivo para dar commit

---
+++ src/VeniceOrders.Infrastructure/Messaging/FakeEventPublisher.cs
@@ -0,0 +1,13 @@
+﻿using VeniceOrders.Domain.Services;
+
+namespace VeniceOrders.Infrastructure.Messaging
+{
+    public sealed class FakeEventPublisher : IEventPublisher
+    {
+        public Task PublishPedidoCriadoAsync(Guid pedidoId, CancellationToken ct)
+        {
+            Console.WriteLine($"[FAKE SERVICE BUS] PedidoCriado -> {pedidoId}");
+            return Task.CompletedTask;
+        }
+    }
+}
